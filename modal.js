dd986d10ba44cd3477f457cd3692c5e7853a04e6
Beck  Lin <becklin@Becks-MacBook-Pro.local>
2021-01-14 21:04:00 +0800

    add image

--- modal.js
+++ modal.js
@@ -1,14 +1,19 @@
 // Get the modal
 var modal = document.getElementById('myModal');
 var modal2 = document.getElementById('myModal2');
+var modal3 = document.getElementById('myModal3');
+
 
 // Get the button that opens the modal
 var btn = document.getElementById('myBtn');
 var btn2 = document.getElementById('myBtn2');
+var btn3 = document.getElementById('myBtn3');
+
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName('close')[0];
 var span2 = document.getElementsByClassName('close')[1];
+var span3 = document.getElementsByClassName('close')[2];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
@@ -16,6 +21,9 @@ btn.onclick = function() {
 };
 btn2.onclick = function() {
   modal2.style.display = 'block';
+};
+btn3.onclick = function() {
+  modal3.style.display = 'block';
 };
 
 // When the user clicks on <span> (x), close the modal
@@ -25,6 +33,9 @@ span.onclick = function() {
 span2.onclick = function() {
   modal2.style.display = 'none';
 };
+span3.onclick = function() {
+  modal3.style.display = 'none';
+};
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
@@ -32,5 +43,8 @@ window.onclick = function(event) {
   }
   if (event.target == modal2) {
     modal2.style.display = 'none';
+  }
+  if (event.target == modal3) {
+    modal3.style.display = 'none';
   }
 };
